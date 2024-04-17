6863a8b8e3cc25e7d247b7f4ac6142326dd2cdd1
NeichS <nachoagusss1@gmail.com>
2024-04-17 20:29:53 -0300

    se agrego map a apps/

--- django-project/project/urls.py
+++ django-project/project/urls.py
@@ -16,7 +16,7 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
-from map import views
+from apps.map import views
 
 urlpatterns = [
     path("admin/", admin.site.urls),
