a099a2350975eb97634564639cd41eca3dcdc62f
Rocio Jofre Olivero <o.rocio@gmail.com>
2024-04-17 21:13:36 -0300

    Se modificaron las variables de entorno y se creo la app user

--- django-project/project/settings.py
+++ django-project/project/settings.py
@@ -40,7 +40,8 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
-    "apps.map"
+    "apps.map",
+    "apps.user"
 ]
 
 MIDDLEWARE = [
@@ -89,11 +90,11 @@ load_dotenv(dotenv_path)
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
-        'NAME': os.getenv("DB_NAME"),
-        'USER': os.getenv("DB_USER"),
-        'PASSWORD': os.getenv("DB_PASSWORD"),
+        'NAME': os.getenv("POSTGRES_DB"),
+        'USER': os.getenv("POSTGRES_USER"),
+        'PASSWORD': os.getenv("POSTGRES_PASSWORD"),
         'HOST': 'localhost',
-        'PORT': os.getenv("DB_PORT") ,
+        'PORT': os.getenv("POSTGRES_PORT") ,
     }
 }
 
