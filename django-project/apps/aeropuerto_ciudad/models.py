72226a2c06231370057bbcedfcbbd23f9a55a4af
Rocio Jofre Olivero <o.rocio@gmail.com>
2024-04-24 20:01:20 -0300

    Se crearon las tablas vuelo,aeropuerto y ciudad

--- django-project/apps/aeropuerto_ciudad/models.py
+++ django-project/apps/aeropuerto_ciudad/models.py
@@ -1,6 +1,11 @@
 from django.db import models
 
-'''
-(Tabla, Encargado) = (Aeropuerto, Tiago), (Ciudad , Tiago)
-'''
+
 # Create your models here.
+class Ciudad(models.Model):
+    nombre = models.CharField(max_length=100)
+
+class Aeropuerto(models.Model):
+    nombre = models.CharField(max_length=100)
+    codigo = models.CharField(max_length=10,unique=True)
+    ciudad = models.ForeignKey(Ciudad, on_delete=models.CASCADE)
--- django-project/apps/vuelo/models.py
+++ django-project/apps/vuelo/models.py
@@ -1,6 +1,12 @@
 from django.db import models
+from apps.aeropuerto_ciudad import models
 
-'''
-(Tabla, Encargado) = (Asiento, Neich), (Pasaje, Neich), (Vuelo, Ro),(Avion, Ro), (Ruta, Tiago),(Escala, Tiago)
-'''
 # Create your models here.
+class Vuelo(models.Model):
+    codigo = models.CharField(max_length=10,unique=True)
+    escala = bool
+    hora = models.datafield()
+    ruta = models.ForeignKey(Ruta, on_delete=models.CASCADE)
+    aeropuertoSalida = models.ForeignKey(models.Aeropuerto, on_delete=models.CASCADE)
+    aeropuertoDestino = models.ForeignKey(models.Aeropuerto, on_delete=models.CASCADE)
+
