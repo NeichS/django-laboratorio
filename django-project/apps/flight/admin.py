2c9640801e4699f79a138d4da42e219c44b4fe02
Nacho Sanchez <nachoagusss1@gmail.com>
2024-05-03 19:42:46 -0300

    se agregaron los modelos al panel de admin

--- django-project/apps/flight/admin.py
+++ django-project/apps/flight/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
 
+from .models import *
+
 # Register your models here.
+
+admin.site.register(Flight)
+admin.site.register(FlightHistory)
+admin.site.register(Itinerary)
+admin.site.register(Plane)
+admin.site.register(Ticket)
